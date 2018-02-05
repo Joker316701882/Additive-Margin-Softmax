c4096d79faa24cf157200e2c605ffeb167b51b8c
Joker316701882 <gezheng121@foxmail.com>
2018-02-05 13:27:10 +0900

    make network same with authors

--- resface.py
+++ resface.py
@@ -1,28 +1,26 @@
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 
-
 '''
 resface20 and resface36 proposed in sphereface and applied in Additive Margin Softmax paper
 '''
 
-
 def prelu(x):
-  alphas = tf.Variable(tf.constant(0.25,dtype=tf.float32,shape=x.get_shape()[-1]),name='prelu_alphas')
+  alphas = tf.Variable(tf.constant(0.25,dtype=tf.float32,shape=[x.get_shape()[-1]]),name='prelu_alphas')
   pos = tf.nn.relu(x)
   neg = alphas * (x - abs(x)) * 0.5
   return pos + neg
 
 def resface_block(lower_input,output_channels,scope=None):
     with tf.variable_scope(scope):
+        net = slim.conv2d(lower_input, output_channels)
         net = slim.conv2d(net, output_channels)
-        net = slim.conv2d(net, output_channels,activation_fn=None)
-        return prelu(lower_input + net)
+        return lower_input + net
 
 def resface_pre(lower_input,output_channels,scope=None):
-    net = slim.conv2d(lower_input, output_channels, stride=2, activation_fn=None, scope=scope)
-    return prelu(lower_input+net)
-  
+    net = slim.conv2d(lower_input, output_channels, stride=2, scope=scope)
+    return net
+
 def resface20(images, keep_probability, 
              phase_train=True, bottleneck_layer_size=512, 
              weight_decay=0.0, reuse=None):
@@ -48,8 +46,8 @@ def resface20(images, keep_probability,
 
     with tf.variable_scope('Logits'):
         #pylint: disable=no-member
-        net = slim.avg_pool2d(net, net.get_shape()[1:3], padding='VALID',
-                              scope='AvgPool')
+        #net = slim.avg_pool2d(net, net.get_shape()[1:3], padding='VALID',
+        #                      scope='AvgPool')
         net = slim.flatten(net)
 
         net = slim.dropout(net, keep_probability, is_training=phase_train,
@@ -57,7 +55,7 @@ def resface20(images, keep_probability,
     
     net = slim.fully_connected(net, bottleneck_layer_size, activation_fn=None, 
             scope='Bottleneck', reuse=False)            
-    return net
+    return net,''
 
 def resface36(images, keep_probability, 
              phase_train=True, bottleneck_layer_size=512, 
@@ -90,7 +88,7 @@ def resface36(images, keep_probability,
                            scope='Dropout')
     net = slim.fully_connected(net, bottleneck_layer_size, activation_fn=None, 
             scope='Bottleneck', reuse=False)    
-    return net
+    return net,''
 
 def inference(image_batch, keep_probability, 
               phase_train=True, bottleneck_layer_size=512, 
@@ -106,10 +104,11 @@ def inference(image_batch, keep_probability,
         'variables_collections': [ tf.GraphKeys.TRAINABLE_VARIABLES ],
     }    
     with slim.arg_scope([slim.conv2d, slim.fully_connected], 
-                         weights_initializer=tf.truncated_normal_initializer(stddev=0.1), 
+                         weights_initializer=tf.truncated_normal_initializer(stddev=0.01), 
                          weights_regularizer=slim.l2_regularizer(weight_decay), 
                          activation_fn=prelu,
                          normalizer_fn=slim.batch_norm,
+                         #normalizer_fn=None,
                          normalizer_params=batch_norm_params):
         with slim.arg_scope([slim.conv2d], kernel_size=3):
             return resface20(images=image_batch, 
