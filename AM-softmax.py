82740253ad686be780a8351bd285c4f5976b8655
GE ZHENG <dsjokerzz@gmail.com>
2018-02-06 13:01:13 +0900

    change default number of m

--- AM-softmax.py
+++ AM-softmax.py
@@ -14,14 +14,14 @@ def AM_logits_compute(bottle_neck, label_batch, args, nrof_classes, lamb):
                   paper, author mentioned that there is no need to use lambda, with directly training using AM-softmax 
                   can also work) 
     '''
-    m = 0.25
+    m = 0.35
     s = 30
     with tf.name_scope('AM_logits'):
         kernel = tf.Variable(tf.truncated_normal([args.embedding_size, nrof_classes]))
-        kernel_len = tf.sqrt(tf.reduce_sum(tf.square(kernel), axis = 0, keep_dims = True)+1e-10)#(1, 10)
-        logits = tf.matmul(bottle_neck, kernel)#(batch_size, 10)
-        bottle_neck_len = tf.sqrt(tf.reduce_sum(tf.square(bottle_neck), axis = 1, keep_dims = True)+1e-10)#(batch_size, 1)
-        cos_theta = logits/(bottle_neck_len*kernel_len)#(batch_size, 10) 表征了每个feature与对应权重的夹角
+        kernel_len = tf.sqrt(tf.reduce_sum(tf.square(kernel), axis = 0, keep_dims = True)+1e-10)
+        logits = tf.matmul(bottle_neck, kernel)
+        bottle_neck_len = tf.sqrt(tf.reduce_sum(tf.square(bottle_neck), axis = 1, keep_dims = True)+1e-10)
+        cos_theta = logits/(bottle_neck_len*kernel_len)
         phi = cos_theta - m 
         label_onehot = tf.one_hot(label_batch, nrof_classes)
         adjust_theta = s * tf.where(tf.equal(label_onehot,1), phi, cos_theta)       
