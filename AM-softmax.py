b00253a46bfa87c514bcd20e57949f1c941b546f
Ge Zheng <gezheng121@foxmail.com>
2018-02-28 15:31:00 +0900

    Update AM-softmax.py

--- AM-softmax.py
+++ AM-softmax.py
@@ -17,8 +17,8 @@ def AM_logits_compute(embeddings, label_batch, args, nrof_classes):
     with tf.name_scope('AM_logits'):
         kernel = tf.Variable(tf.truncated_normal([args.embedding_size, nrof_classes]))
         kernel_norm = tf.nn.l2_normalize(kernel, 0, 1e-10, name='kernel_norm')
-        cos_theta = tf.matmul(embeddings, kernel_norm)#(batch_size, nrof_classes) 表征了每个feature与对应权重的夹角
-        cos_theta = tf.clip_by_value(cos_theta, -1,1)
+        cos_theta = tf.matmul(embeddings, kernel_norm)
+        cos_theta = tf.clip_by_value(cos_theta, -1,1) # for numerical steady
         phi = cos_theta - m 
         label_onehot = tf.one_hot(label_batch, nrof_classes)
         adjust_theta = s * tf.where(tf.equal(label_onehot,1), phi, cos_theta)
